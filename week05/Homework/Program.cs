9193a6ba06fc95d61f95b94a4c1bb9c40bf8c3be
Murokarg <edwin.rene.rodriguez.ortiz@gmail.com>
2025-10-13 12:07:00 -0600

    W05 assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment cs1 = new Assignment("Gordon James", "Division");
+        Console.WriteLine(cs1.GetSummary());
+
+        MathAssignment cs2 = new MathAssignment("Jonathan Reyes", "Calculus", "8.5", "5-20");
+        Console.WriteLine(cs2.GetSummary());
+        Console.WriteLine(cs2.GetHomeworkList());
+
+        WritingAssignment cs3 = new WritingAssignment("Julia Roberts", "English Literature", "Studies in ProtoEnglish");
+        Console.WriteLine(cs3.GetSummary());
+        Console.WriteLine(cs3.GetWritingInformation());
     }
 }
